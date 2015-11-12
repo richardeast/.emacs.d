3ed4aa551fcd3b5daabda08823c1660a2dd328b2
unknown <eastric@W5093661.uk.experian.local>
2015-11-12 17:25:12 +0000

    Updates some of the programming packages and improved the notes

--- init.el
+++ init.el
@@ -4,8 +4,9 @@
 ;; Package Archives.  This must come before configurations of
 ;; installed packages.
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 (package-initialize)
 
 (require 'org)
